5b592c40fbad8b4b17ded8160d743f4ba35781ec
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-01-16 16:32:56 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Ange ett tal");
+    string input = Console.ReadLine();
+
+    // konvertera det som skrivs in till ett heltal
+    if (int.TryParse(input, out int tal))
+    {
+        if (tal > 5)
+        {
+            Console.WriteLine("Högre än 5");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ogiltigt tal. Försök igen.");
+       
+    }
+}
